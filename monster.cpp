beab3b12db82977b3e2107b69550d88561d6165e
Brian Magana <bmagana5@csub.edu>
2019-11-23 19:12:08 -0800

    updated monster.cpp: moved some functions to own header and cpp files

--- monster.cpp
+++ monster.cpp
@@ -22,8 +22,9 @@
 #include <X11/keysym.h>
 #include <GL/glx.h>
 #include "log.h"
-//#include "ppm.h"
 #include "fonts.h"
+#include "defs.h"
+#include "timers.h"
 #include "Image.h"
 #include "Player.h"
 #include "Obstacle.h"
@@ -32,20 +33,6 @@
 #include </usr/include/AL/alut.h>
 #endif
 
-//defined types
-typedef double Flt;
-typedef double Vec[3];
-typedef Flt	Matrix[4][4];
-
-//macros
-#define rnd() (((double)rand())/(double)RAND_MAX)
-#define random(a) (rand()%a)
-#define MakeVector(x, y, z, v) (v)[0]=(x),(v)[1]=(y),(v)[2]=(z)
-#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
-#define VecDot(a,b)	((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
-#define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
-			     (c)[1]=(a)[1]-(b)[1]; \
-(c)[2]=(a)[2]-(b)[2]
 //constants
 const float timeslice = 1.0f;
 const float gravity = -0.2f;
@@ -69,22 +56,11 @@ extern void cleanupAudio(ALuint *, ALuint *, int);
 //Setup timers
 //clock_gettime(CLOCK_REALTIME, &timePause);
 const double physicsRate = 1.0 / 30.0;
-const double oobillion = 1.0 / 1e9;
 struct timespec timeStart, timeEnd, timeCurrent;
 struct timespec timePause, moveTime;
 double physicsCountdown=0.0;
 double timeSpan=0.0;
 unsigned int upause=0;
-double timeDiff(struct timespec *start, struct timespec *end) {
-    return (double)(end->tv_sec - start->tv_sec ) +
-	(double)(end->tv_nsec - start->tv_nsec) * oobillion;
-}
-void timeCopy(struct timespec *dest, struct timespec *source) {
-    memcpy(dest, source, sizeof(struct timespec));
-}
-void recordTime(struct timespec *t) {
-	clock_gettime(CLOCK_REALTIME, t);	
-}
 //-----------------------------------------------------------------------------
 
 //define audio globals
