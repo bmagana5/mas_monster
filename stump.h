6151fa04270c1a4af86dd2a7c106a69e4e3eb466
KrystalMae Raynes <kraynes@odin.cs.csubak.edu>
2019-12-08 19:18:32 -0800

    stump header

---
+++ stump.h
@@ -0,0 +1,27 @@
+#ifndef STUMP_H
+#define STUMP_H
+
+#include <GL/glx.h>
+#include "Image.h" 
+#include "Global.h" 
+#include "../defs.h" 
+class Stump 
+{
+    public:
+        int move, currentFrame; //frame_count;
+        float delay;
+        GLuint glTexture;
+        Image img;
+        Vec pos;
+        Vec vel;
+        float size_rate;
+        int height;
+        int width;
+        int size_with_screen;
+
+        Stump(const char *file);
+        Stump(const char *file, const Global *g);
+        void resize(Global *g);
+
+};
+#endif
